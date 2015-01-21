e67ddd055bf4674a4636893882397e3aa7dcddc8
Henry Crute <henrycrute@gmail.com>
2015-01-20 18:17:25 -0800

    added buffer library for received data, added state machine functionality for receiving, storing, and sending data

--- ESP8266/ESP8266_SDK/uart_rxtx_PROJECT/user/send_recv_port.h
+++ ESP8266/ESP8266_SDK/uart_rxtx_PROJECT/user/send_recv_port.h
@@ -4,16 +4,39 @@
  * 	
  * 	header file for sending through wifi, and receiving through uart
  * 
- *  
- * 
- * 
- * 
  */
 
 #ifndef __SEND_RECV_PORT_H
 #define __SEND_RECV_PORT_H
 
+//for the os_event_t type
+#include "user_interface.h"
+
+//# defines for the queue tasking config
+#define recv_messagePrio		0
+#define recv_messageQueueLen	64
+
+#define store_messagePrio		1
+#define store_messageQueueLen	1
+
+#define send_messagePrio		2
+#define send_messageQueueLen	1
+
+//receive store send states
+typedef enum {
+  receive_idle,
+  receive_message,
+  store,
+  send
+}rss_state;
+
 //prototype definitions start for library
 
+//init the two recv and send messages
+void send_recv_init(void);
+//receiving messages via uart
+void recv_message(os_event_t *events);
+//sending messages via wifi
+void send_message(os_event_t *events);
 
 #endif
