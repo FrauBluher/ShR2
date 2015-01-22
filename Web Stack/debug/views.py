a834f2ffcc2b2fddd2a64f062b99702a6d916f65
Bryan Smith <brabsmit@ucsc.edu>
2015-01-22 17:30:59 +0000

    Webserver: improved debug flow

--- Web Stack/debug/views.py
+++ Web Stack/debug/views.py
@@ -22,6 +22,9 @@ class DatagenForm(forms.Form):
 class DatadelForm(forms.Form):
    device = DeviceModelChoiceField(label='Device', queryset=Device.objects.all())
 
+class DevForm(forms.Form):
+   method = forms.ChoiceField(choices=(('datagen','datagen'),('datadel','datadel')))
+
 @csrf_exempt
 def gitupdate(request):
     if request.method == 'POST':
@@ -41,6 +44,7 @@ def echo_args(request, args):
    return HttpResponse(status=200)
    
 def datagen(request):
+   success = ""
    if request.method == 'POST':
       form = DatagenForm(request.POST)
       if form.is_valid():
@@ -56,23 +60,41 @@ def datagen(request):
                  event = Event(device=device, timestamp=i*1000, wattage=wattage, appliance=appliance)
                  event.save()
                  events.append("{0}: {1}: {2} - {3}".format(event.device, event.timestamp, event.wattage, event.appliance))
-         return HttpResponse(events)
+         success = "Added %i events successfully" % (stop - start)
    else:
       form = DatagenForm()
    title = "Debug - Data Generation"
-   description = "Use this form to submit random generated data for the device chosen."
-   return render(request, 'debug.html', {'title':title,'description',description,'form':form})
+   description = "Use this form to submit random generated data with 1s resolution for the device chosen."
+   return render(request, 'debug.html', {'title':title,'description':description,'form':form, 'success':success})
 
 def datadel(request):
-  if request.method == 'POST':
-    form = DatadelForm(request.POST)
-    if form.is_valid():
-       device = form.cleaned_data['device']
-       events = Event.objects.filter(device=device)
-       events.delete()
-  else:
-    form = DatadelForm()
-  title = "Debug - Data Deletion"
-  description = "Use this form to delete data for the device chosen."
-  return render(request, 'debug.html', {'title':title,'description',description,'form':form})
-      
+   success = ""
+   if request.method == 'POST':
+      form = DatadelForm(request.POST)
+      count = 0
+      if form.is_valid():
+         device = form.cleaned_data['device']
+         events = Event.objects.filter(device=device)
+         count = len(events)
+         events.delete()
+         success = "Deleted %i events successfully" % count
+   else:
+      form = DatadelForm()
+   title = "Debug - Data Deletion"
+   description = "Use this form to delete data for the device chosen."
+   return render(request, 'debug.html', {'title':title,'description':description,'form':form, 'success':success})
+
+def dev(request):
+   methods = {'datagen':datagen, 'datadel':datadel}
+   if request.method == 'POST':
+      form = DevForm(request.POST)
+      if form.is_valid():
+         method = form.cleaned_data['method']
+         if method in methods:
+            return HttpResponse(methods[method](request))
+         else: return HttpResponse(status=404)
+   else:
+      form = DevForm()
+   title = "Development - Method Select"
+   description = "Select a debug method"
+   return render(request, 'debug.html', {'title':title,'description':description,'form':form})
--- Web Stack/seads/urls.py
+++ Web Stack/seads/urls.py
@@ -28,6 +28,5 @@ urlpatterns = [
     url(r'^gitupdate/$', 'debug.views.gitupdate'),
     url(r'^echo/', 'debug.views.echo'),
     url(r'^echo/(?P<args>\w+)$', 'debug.views.echo_args'),
-    url(r'^datagen/$', 'debug.views.datagen'),
-    url(r'^datadel/$', 'debug.views.datadel')
+    url(r'^dev/$', 'debug.views.dev'),
 ]
