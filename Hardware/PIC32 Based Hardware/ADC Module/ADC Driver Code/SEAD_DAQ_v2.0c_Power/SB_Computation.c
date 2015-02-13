8e1d3354dff0eb73e5798739d7fc6b3a56dc72f7
Henry Crute <henrycrute@gmail.com>
2015-02-13 15:28:09 -0800

    Added truerms calculation framework

--- Hardware/PIC32 Based Hardware/ADC Module/ADC Driver Code/SEAD_DAQ_v2.0c_Power/SB_Computation.c
+++ Hardware/PIC32 Based Hardware/ADC Module/ADC Driver Code/SEAD_DAQ_v2.0c_Power/SB_Computation.c
@@ -5,10 +5,21 @@
  * @brief 	Implimentations of computations on type SampleBuffer
  */
 
+#include <math.h>
+
 #include "DMA_Transfer.h"
 
 
 //takes the RMS of the given SampleBuffer
+//must do something about the DC offset on the ADC
+//Also must take into account the signed-ness of the data
+//also this is a 24bit adc and we are reading in unsigned bytes
+
 uint8_t SB_RMS(SampleBuffer *buffer) {
-    return buffer->BufferArray[0];
+        uint32_t rawrms = 0;
+        uint32_t rms = 0;
+        for (uint8_t i = 0; i < 130; i++) {
+                rawrms += buffer->BufferArray[i] * buffer->BufferArray[i];
+        }
+        return sqrt(rawrms / 130);
 }
