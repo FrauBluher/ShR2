ce61197b513eeb576dbe87c36564956108d90bef
A.J. Ringer <ajringer@ucsc.edu>
2015-02-17 16:11:13 -0800

    Fixed buffering and added channel specific RMS

--- Hardware/PIC32 Based Hardware/ADC Module/ADC Driver Code/SEAD_DAQ_v2.0c_Power/SB_Computation.c
+++ Hardware/PIC32 Based Hardware/ADC Module/ADC Driver Code/SEAD_DAQ_v2.0c_Power/SB_Computation.c
@@ -9,16 +9,25 @@
 
 #include "DMA_Transfer.h"
 
+#define WINDOW_SIZE 130
+#define CHANNEL 1
+
 
 //takes the RMS of the given SampleBuffer
 //must do something about the DC offset on the ADC
 //Also must take into account the signed-ness of the data
 
-uint32_t SB_RMS(SampleBuffer *buffer) {
-        uint32_t rawrms = 0;
-        uint16_t i;
-        for (i = 0; i < 130; i++) {
-                rawrms += *(buffer->BufferArray + i) * *(buffer->BufferArray + i);
-        }
-        return sqrt(rawrms / 130);
+uint32_t SB_RMS(SampleBuffer *buffer)
+{
+	uint64_t rawrms = 0;
+	uint16_t i;
+	for (i = 0; i < WINDOW_SIZE; i++) {
+		// TODO: optimize with a single 32 bit load
+		uint64_t value =
+			buffer->BufferArray[(3*CHANNEL+1) + i * 13] |
+			buffer->BufferArray[(3*CHANNEL+2) + i * 13] << 1 |
+			buffer->BufferArray[(3*CHANNEL+3) + i * 13] << 2;
+		rawrms += value * value;
+	}
+	return sqrt(rawrms / WINDOW_SIZE);
 }
