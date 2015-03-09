05e94859e6b1fbad1856c01b31ca9d10127513d2
Pavlo Manovi <me@pavlo.me>
2015-03-09 13:05:40 -0700

    More board layout work.

--- Hardware/PIC32 Based Hardware/ADC Module/ADC Driver Code/SEAD_DAQ_v2/build/default/production/ADCModuleBoard.o.d
+++
@@ -1,5 +0,0 @@
-build/default/production/ADCModuleBoard.o:  \
-ADCModuleBoard.c  \
- ADCModuleBoard.h  \
-DMA_Transfer.h  \
-MCP391x.h 
--- Hardware/PIC32 Based Hardware/ADC Module/ADC Driver Code/SEAD_DAQ_v2/build/default/production/DAQ.o.d
+++
@@ -1,5 +0,0 @@
-build/default/production/DAQ.o:  \
-DAQ.c  \
-ADCModuleBoard.h  \
-DMA_Transfer.h  \
- MCP391x.h 
