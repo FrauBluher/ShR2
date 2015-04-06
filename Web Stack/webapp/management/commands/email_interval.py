f0e809392727eaf370639f3952526c34b44a715e
Bryan Smith <brabsmit@ucsc.edu>
2015-04-06 15:08:11 +0000

    Webserver: Rendering graphs for interval emails

--- Web Stack/webapp/management/commands/email_interval.py
+++ Web Stack/webapp/management/commands/email_interval.py
@@ -9,14 +9,61 @@ from matplotlib import pyplot as plt
 import numpy as np
 from django.template import Context, Template
 import os
+from time import gmtime, strftime
+from influxdb import client as influxdb
+from microdata.models import Device
+from sets import Set
+import re
+import datetime
+import numpy as np
+from calendar import monthrange
+import random
 
-def render_chart(user):
-   x = np.linspace(0, 10)
-   with plt.style.context('fivethirtyeight'):
-      plt.plot(x, np.sin(x) + x + np.random.randn(50))
-      plt.plot(x, np.sin(x) + 0.5 * x + np.random.randn(50))
-      plt.plot(x, np.sin(x) + 2 * x + np.random.randn(50))
-   plt.savefig(settings.STATIC_PATH+'/webapp/img/'+user.username+'_plot.png')
+# This command will generate a figure in the STATIC_PATH for every user.
+   
+def render_chart(user, interval):
+   date_today = datetime.datetime.today()
+   date_gmtime = gmtime()
+   randbits = str(random.getrandbits(128))
+   start = 'now() - 1w'
+   unit = 'h'
+   if interval['pk'] == 13:
+      start = 'now() - 1M'
+      unit = 'd'
+      
+   stop = 'now()'
+   db = influxdb.InfluxDBClient('localhost',8086,'root','root','seads')
+   fig = plt.figure(figsize=(10, 5), dpi=100)
+   plt.ylabel('Watts')
+   for device in Device.objects.filter(owner=user):
+      points = {}
+      result = db.query('list series')[0]
+      appliances = Set()
+      for series in result['points']:
+         rg = re.compile('device.'+str(device.serial))
+         if re.match(rg, series[1]):
+            appliance = series[1].split('device.'+str(device.serial)+'.')
+            if (len(appliance) < 2): continue
+            else: appliances.add(appliance[-1])
+      for appliance in appliances:
+         query = 'select * from 1'+unit+'.device.'+str(device.serial)+'.'+appliance+' where time > '+start+' and time < '+stop
+         group = db.query(query)
+         if (len(group)): group = group[0]['points']
+         for s in group:
+            if s[0] in points:
+               points[s[0]] += s[2]
+            else:
+               points[s[0]] = s[2]
+      y = []
+      for key, value in points.iteritems():
+         y.append(value)
+      x = np.array([date_today - datetime.timedelta(hours=i) for i in range(len(y))])
+      if interval['pk'] == 13:
+         x = np.array([date_today - datetime.timedelta(days=i) for i in range(len(y))])
+      plt.plot(x, y)
+   plt.savefig(settings.STATIC_PATH+'/webapp/img/' + interval['alt-text'] + '_' + str(user.pk)+'_'+randbits+'_plot.png')
+   return [randbits, strftime("%a, %d %b %Y %H:%M:%S +0000", date_gmtime)]
+         
 
 class Command(BaseCommand):
    args = '<weekly, monthly>'
@@ -38,15 +85,17 @@ class Command(BaseCommand):
                   text = ""
                   with open(settings.STATIC_PATH+"/webapp/email/consumption_details.txt", "r") as f:
                      text = f.read()
-                  render_chart(user)
+                  randbits, str_time = render_chart(user, intervals[args[0]])
                   template = Template(text)
                   context = Context({
+                             'time': str_time,
                              'organization': settings.ORG_NAME,
                              'base_url': settings.BASE_URL,
                              'interval': interval,
                              'interval_lower': interval.lower(),
+                             'period': interval.lower()[:-2],
                              'user_firstname': user.first_name,
-                             'plot_location': 'http://'+settings.BASE_URL+'/static/webapp/img/'+user.username+'_plot.png'
+                             'plot_location': 'http://'+settings.BASE_URL+'/static/webapp/img/'+ intervals[args[0]]['alt-text'] + '_' + str(user.pk)+'_'+randbits+'_plot.png'
                            })
                   message = {
                      'Subject': {
