04dae7695fab1430ba68801b4a7c22f270364ffd
Bryan Smith <brabsmit@ucsc.edu>
2015-05-27 04:19:22 +0000

    Webserver: Updated python deploy script to reflect change of directory

--- Web Stack/custom_config.py
+++ Web Stack/custom_config.py
@@ -3,14 +3,13 @@ from os.path import expanduser
 
 def main():
 	cwd = os.getcwd()
-	home = expanduser("~")
-	with open(home+"/nginx.conf", "w") as newfile:
+	with open("/srv/nginx.conf", "w") as newfile:
 		with open("deploy/nginx.conf", "r") as oldfile:
 			for line in oldfile:
 				newline = line.replace('           alias "/home/ubuntu/seads/Web Stack/webapp/static/";','           alias "'+cwd+'/webapp/static/";')
 				newfile.write(newline)
 
-	with open(home+"/uwsgi.ini", "w") as newfile:
+	with open("/srv/uwsgi.ini", "w") as newfile:
 		with open("deploy/uwsgi.ini", "r") as oldfile:
 			for line in oldfile:
 				newline = line.replace('chdir=/home/ubuntu/seads/Web Stack/','chdir='+cwd)
