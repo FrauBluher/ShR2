02dade1cfb0ad1845f9b74fb6f0230ef8ba5cbd7
Bryan Smith <brabsmit@ucsc.edu>
2015-05-29 00:26:43 +0000

    Webserver: migrated django database from sqlite to mysql

--- Web Stack/seads/settings.py
+++ Web Stack/seads/settings.py
@@ -137,8 +137,12 @@ WSGI_APPLICATION = 'seads.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db',
+        'USER': 'root',
+        'PASSWORD': 'teammantey',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
