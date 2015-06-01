027afb184e15353263d76fd38279b8535d1e8e3e
Ubuntu <ubuntu@ip-172-31-43-199.us-west-2.compute.internal>
2015-06-01 21:57:25 +0000

    Webserver: altered database deploy script

--- Web Stack/deploy_database.sh
+++ Web Stack/deploy_database.sh
@@ -3,17 +3,11 @@
 # check if influxdb is installed. If not, download and install
 
 dpkg -s influxdb >/dev/null 2>&1 && {
-	echo "influxdb is already installed."
+        echo "influxdb is already installed."
 } || {
-	wget https://s3.amazonaws.com/influxdb/influxdb_latest_amd64.deb
+        wget https://s3.amazonaws.com/influxdb/influxdb_latest_amd64.deb
 
-	sudo dpkg -i influxdb_latest_amd64.deb
+        sudo dpkg -i influxdb_latest_amd64.deb
 
-	rm influxdb_latest_amd64.deb
+        rm influxdb_latest_amd64.deb
 }
-
-# create seads database
-
-curl -X POST 'http://localhost:8086/db?u=root&p=root' \
-  -d '{"name": "seads"}'
-
