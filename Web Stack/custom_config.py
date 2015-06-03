666550d5cbb7a79737ccea41bac04db3b41c8210
Bryan Smith <brabsmit@ucsc.edu>
2015-06-03 22:48:43 +0000

    Webserver: added link to static documentation

--- Web Stack/custom_config.py
+++ Web Stack/custom_config.py
@@ -8,6 +8,11 @@ def main():
 			for line in oldfile:
 				newline = line.replace('           alias "/home/ubuntu/seads/Web Stack/webapp/static/";','           alias "'+cwd+'/webapp/static/";')
 				newfile.write(newline)
+	with open("/srv/nginx.conf", "w") as newfile:
+		with open("deploy/nginx.conf", "r") as oldfile:
+			for line in oldfile:
+				newline = line.replace('           alias "/home/ubuntu/seads/Web Stack/documentation/_build/html";','           alias "'+cwd+'documentation/_build/html";')
+				newfile.write(newline)
 
 	with open("/srv/uwsgi.ini", "w") as newfile:
 		with open("deploy/uwsgi.ini", "r") as oldfile:
