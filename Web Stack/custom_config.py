3a979482e56bf2800510ce47f9074be50834e928
Bryan Smith <brabsmit@ucsc.edu>
2015-06-10 17:26:34 +0000

    Webserver: fixed nginx config script for deploy

--- Web Stack/custom_config.py
+++ Web Stack/custom_config.py
@@ -3,13 +3,13 @@ from os.path import expanduser
 
 def main():
 	cwd = os.getcwd()
-	with open("/srv/nginx.conf", "w") as newfile:
+	with open("/srv/nginx1.conf", "w") as newfile:
 		with open("deploy/nginx.conf", "r") as oldfile:
 			for line in oldfile:
 				newline = line.replace('           alias "/home/ubuntu/seads/Web Stack/webapp/static/";','           alias "'+cwd+'/webapp/static/";')
 				newfile.write(newline)
 	with open("/srv/nginx.conf", "w") as newfile:
-		with open("deploy/nginx.conf", "r") as oldfile:
+		with open("/srv/nginx1.conf", "r") as oldfile:
 			for line in oldfile:
 				newline = line.replace('           alias "/home/ubuntu/seads/Web Stack/documentation/_build/html";','           alias "'+cwd+'documentation/_build/html";')
 				newfile.write(newline)
